8c4fbfce5ae5264adb3148ab1a49baa55dec4e6c
bakhrul <35515737+bakhrul@users.noreply.github.com>
2026-06-08 23:12:16 +0700

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'app/',
+  dir: {
+    public: '../public'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
